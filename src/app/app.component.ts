ffefd9dbacdba3b859d82a1e305d202985ba06df
Mahendranath Reddy <mahendranath.reddy22@gmail.com>
2018-02-15 23:12:55 +0530

    NgIFand NFOR Demo ADDand Delete
    Push and Delete Logic Implementation is working Here

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,38 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title= ' Details'
+  subtitle = 'Data';
+  selectedlist ="Selected Data";
+  dl = [
+  {id:1, name:"A" },
+  {id:2, name:"B" },
+  {id:3, name:"C" },
+  {id:4, name:"D" },
+  {id:5, name:"E" },
+  
+  ];
+  
+    selectedItems:any=[];
+     selectlist(dl:any){
+	  console.log(dl);
+	  this.selectedItems.push(dl);
+	  
+  }
+  
+  removelist(dl:any){
+    var b = (this.selectedItems.indexOf(dl));
+    if (b > -1) {
+      this.selectedItems.splice(b, 1);
+  }
+
+  //  this.selectedItems.splice(this.selectedItems.index(b ,1));
+    //this.selectedItems.splice(this.selectedItems.indexOf());
+    //this.selectedItems.findIndex(this.selectedItems);
+    //this.selectedItems.splice(this.selectedItems.indexOf(dl));
+	  //this.selectedItems.splice(this.selectedItems.indexOf(dl, 1));
+	  
+  }
 }
